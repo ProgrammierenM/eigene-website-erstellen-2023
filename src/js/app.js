2e427d85f94b523e90adb22498be5cc4e0594b5d
Mario <programmierenmitmario@gmail.com>
2023-05-19 12:28:34 +0200

    typewriter effect

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ class PageHandler {
     }
 
     this.setThemeIcon();
+    this.typeWriterManager();
 
     document.querySelector("#colorSwitch").addEventListener("click", () => {
       document.documentElement.classList.toggle("dark");
@@ -32,6 +33,49 @@ class PageHandler {
         <img src="/icons/moon.svg" alt="sun" class="w-6 h-6" />
       `;
     }
+  }
+
+  typeWriterManager() {
+    let dynamicHeaderElement = document.querySelector("#dynamic-header-text");
+    if (dynamicHeaderElement === null) return;
+
+    let dynamicHeaderTextArray = ["Design", "Kaffee", "Pizza"];
+    let i = 0;
+    let currentText = dynamicHeaderTextArray[i];
+    let currentWord = dynamicHeaderTextArray[i];
+    let isDeleting = false;
+
+    let typeWriter = () => {
+      currentWord = dynamicHeaderTextArray[i];
+
+      if (isDeleting) {
+        currentText = currentWord.substring(0, currentText.length - 1);
+      }
+
+      if (!isDeleting) {
+        currentText = currentWord.substring(0, currentText.length + 1);
+      }
+
+      dynamicHeaderElement.innerHTML = currentText;
+
+      if (!isDeleting && currentText === currentWord) {
+        isDeleting = true;
+        setTimeout(typeWriter, 3000);
+      } else if (isDeleting && currentText === "") {
+        isDeleting = false;
+        i++;
+
+        if (i === dynamicHeaderTextArray.length) {
+          i = 0;
+        }
+
+        setTimeout(typeWriter, 500);
+      } else {
+        setTimeout(typeWriter, 100);
+      }
+    };
+
+    typeWriter();
   }
 }
 
